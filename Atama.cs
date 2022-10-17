9d840129c2ad9dcd489a83e1874dd8bf651b4a24
mehmetrafii <115981843+mehmetrafii@users.noreply.github.com>
2022-10-17 11:36:54 +0300

    111

---
+++ Atama.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atama
+{
+    public class Atama
+    {
+       public void Rastgele()
+        {
+            string[] SinifListesi = {"Salih","Cihat Han","Neval","İlker","Devrim","Muhammet",
+            "Burak","Bedirhan","Mısra","Büşra","Mert K","Mert T","Sena","Erhan","Berkcan"};
+            Random rastgeleSayi=new Random();
+            
+            for (int i = 0; i < SinifListesi.Length; i++)
+            {
+                int sectigimSayi=rastgeleSayi.Next(0,SinifListesi.Length-i);
+                Console.WriteLine("{0} {1}", (i + 1),(SinifListesi[sectigimSayi]));
+                SinifListesi[sectigimSayi] = SinifListesi[SinifListesi.Length - 1 - i];
+
+            }
+
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarpimTablosu1(10,10);
+            Console.ReadLine();
+        }
+        static void CarpimTablosu(int carpan, int adim)
+        {
+
+            int sonuc = 0;
+            for (int i = 1; i <= adim; i++)
+            {
+
+                sonuc = i * carpan;
+                Console.WriteLine("{0}x{1}={2}", i, carpan, sonuc);
+            }
+
+        }
+
+
+
+        static void CarpimTablosu1(int carpan, int adim)
+        {
+            int sonuc = 0;
+            ;
+            for (int i = 1; i < carpan; i++)
+            {
+                for (int j = 1; j < adim; j++)
+                {
+                    sonuc = i * j;
+                    if (sonuc >= 10)
+                    {
+                        Console.Write("{0}*{1}= " + sonuc + "   ", j, i);
+                    }
+                    else
+                    {
+                        Console.Write("{0}*{1}= " + sonuc + "    ", j, i);
+                    }
+                }
+                Console.WriteLine();
+            }
+
+
+
+        }
+
+    }
+}
